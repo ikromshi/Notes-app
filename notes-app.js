7407708cd90fd7641ba5d3bb9fd15c30a8aec752
ikromshi <numanovikrom454@gmail.com>
2021-12-31 01:18:01 -0500

    setting up the basics for Notes-app

---
+++ notes-app.js
@@ -0,0 +1,45 @@
+// DOM - Document Object Model
+
+const notes = [{
+    title: "Mo next trip",
+    body: "I want to got to Spain"
+}, {
+    title: "Habits to work on",
+    body: "Exercise. Eat a bit better"
+}, {
+    title: "Office modification",
+    body: "Get a new chair"
+}]
+
+
+
+// RENDERING FILTERED DATA
+
+const filters = {
+    searchText: ""
+}
+// Checks if the user input is in the map
+const renderNotes = function(notes, filters) {
+    const filteredData = notes.filter(function(item) {
+        return item.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    // Clears the div
+    document.querySelector("#notes-div").innerHTML = ""
+
+    // Writes the results in the div
+    filteredData.forEach(function(item){
+        const newEl = document.createElement("p")
+        newEl.textContent = item.title
+        document.querySelector("#notes-div").appendChild(newEl)
+    })
+}
+
+document.querySelector("#add-todo").addEventListener("input", function(event) {
+    filters.searchText = event.target.value
+    renderNotes(notes, filters)
+})
+
+
+
+
+
